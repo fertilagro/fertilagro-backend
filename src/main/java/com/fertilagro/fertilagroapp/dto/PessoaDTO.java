1c95a9c86991b04383f65bb9270d38580dfe64ec
TIAGO RIBEIRO <77054363+tiagoribas2021@users.noreply.github.com>
2023-11-13 09:04:23 -0300

    ajuste

--- src/main/java/com/fertilagro/fertilagroapp/dto/PessoaDTO.java
+++ src/main/java/com/fertilagro/fertilagroapp/dto/PessoaDTO.java
@@ -2,57 +2,24 @@ package com.fertilagro.fertilagroapp.dto;
 
 
 import com.fertilagro.fertilagroapp.entities.Endereco;
-import com.fertilagro.fertilagroapp.entities.PessoaVO;
 import com.fertilagro.fertilagroapp.enumerador.StatusEnum;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class PessoaDTO {
+	
 	private Integer id;
 	private String razaoSocial;
 	private StatusEnum status;	
 	private Endereco endereco;
-	public PessoaDTO() {
-		
-	}
-	
-	public PessoaDTO(PessoaVO entity) {
-		this.id = entity.getId();
-		this.razaoSocial = entity.getRazaoSocial();
-		this.status = entity.getStatus();
-		this.endereco = entity.getEndereco();
-	}
 
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getRazaoSocial() {
-		return razaoSocial;
-	}
-
-	public void setRazaoSocial(String razaoSocial) {
-		this.razaoSocial = razaoSocial;
-	}
-
-	public StatusEnum getStatus() {
-		return status;
-	}
-
-	public void setStatus(StatusEnum status) {
-		this.status = status;
-	}
-
-	public Endereco getEndereco() {
-		return endereco;
-	}
-
-	public void setEndereco(Endereco endereco) {
-		this.endereco = endereco;
-	}
-
-	
 }
--- src/main/java/com/fertilagro/fertilagroapp/entities/PessoaVO.java
+++ src/main/java/com/fertilagro/fertilagroapp/entities/PessoaVO.java
@@ -1,85 +1,44 @@
 package com.fertilagro.fertilagroapp.entities;
 
-import java.util.Objects;
+import org.hibernate.annotations.DynamicInsert;
 
 import com.fertilagro.fertilagroapp.enumerador.StatusEnum;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "pessoa")
+@DynamicInsert
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class PessoaVO {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+    @Column(name = "RAZAOSOCIAL")
 	private String razaoSocial;
+	
+    @Column(name = "STATUS")
 	private StatusEnum status;
+	
 	@Embedded
 	Endereco endereco;
-	
-	public PessoaVO () {
-		
-	}
-	
-	public PessoaVO(Integer id, String razaoSocial, StatusEnum status, Endereco endereco) {
-		this.id = id;
-		this.razaoSocial = razaoSocial;
-		this.status = status;
-		this.endereco = endereco;
-	}
 
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getRazaoSocial() {
-		return razaoSocial;
-	}
-
-	public void setRazaoSocial(String razaoSocial) {
-		this.razaoSocial = razaoSocial;
-	}
-
-	public StatusEnum getStatus() {
-		return status;
-	}
-
-	public void setStatus(StatusEnum status) {
-		this.status = status;
-	}
-
-	public Endereco getEndereco() {
-		return endereco;
-	}
-
-	public void setEndereco(Endereco endereco) {
-		this.endereco = endereco;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id, status);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		PessoaVO other = (PessoaVO) obj;
-		return Objects.equals(id, other.id) && status == other.status;
-	}
 	
 }
