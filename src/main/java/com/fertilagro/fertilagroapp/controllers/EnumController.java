1b915a8561b3edd41ab025081fda634ee317e9ea
TIAGO RIBEIRO <77054363+tiagoribas2021@users.noreply.github.com>
2023-11-18 11:11:55 -0300

    configurando os enuns

--- src/main/java/com/fertilagro/fertilagroapp/controllers/EnumController.java
+++ src/main/java/com/fertilagro/fertilagroapp/controllers/EnumController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Set;
 
 import org.reflections.Reflections;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,19 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fertilagro.fertilagroapp.util.EnumInterface;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/enums")
 public class EnumController {
 
 	@GetMapping()
-	private List<EnumInterface> buscarTodos(@RequestParam("nome") String nome) {
+	private List<EnumInterface> buscarTodos(@RequestParam("descricao") String descricao) {
 		List<EnumInterface> listaEnuns = null;
 		var reflections = new Reflections("com.fertilagro.fertilagroapp.enumerador");
 		Set<Class<? extends EnumInterface>> todasClasses = reflections.getSubTypesOf(EnumInterface.class);
 		
 		Class<? extends EnumInterface> enumeradorEnum = null;
 		for (Class<? extends EnumInterface> class1 : todasClasses) {
-			if(nome.equals(class1.getSimpleName())) {
+			if(descricao.equals(class1.getSimpleName())) {
 				enumeradorEnum = class1;
 				break;
 			}
