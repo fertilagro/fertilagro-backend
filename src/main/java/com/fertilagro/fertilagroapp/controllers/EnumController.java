bf598ba833586af83c6cdd04ce27a4a7288dedf7
TIAGO RIBEIRO <77054363+tiagoribas2021@users.noreply.github.com>
2023-11-18 09:47:02 -0300

    .

--- src/main/java/com/fertilagro/fertilagroapp/controllers/EnumController.java
+++ src/main/java/com/fertilagro/fertilagroapp/controllers/EnumController.java
@@ -1,9 +1,13 @@
 package com.fertilagro.fertilagroapp.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
+import org.reflections.Reflections;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fertilagro.fertilagroapp.util.EnumInterface;
@@ -13,13 +17,26 @@ import com.fertilagro.fertilagroapp.util.EnumInterface;
 public class EnumController {
 
 	@GetMapping()
-	private List<EnumInterface> buscarTodos() {
+	private List<EnumInterface> buscarTodos(@RequestParam("nome") String nome) {
+		List<EnumInterface> listaEnuns = null;
+		var reflections = new Reflections("com.fertilagro.fertilagroapp.enumerador");
+		Set<Class<? extends EnumInterface>> todasClasses = reflections.getSubTypesOf(EnumInterface.class);
 		
-		
-		
-		
-		
-		return null;
+		Class<? extends EnumInterface> enumeradorEnum = null;
+		for (Class<? extends EnumInterface> class1 : todasClasses) {
+			if(nome.equals(class1.getSimpleName())) {
+				enumeradorEnum = class1;
+				break;
+			}
+			
+		}
+		if(enumeradorEnum != null) {
+			listaEnuns = new ArrayList<>();
+			for(EnumInterface constant : enumeradorEnum.getEnumConstants()){
+				listaEnuns.add(constant);
+			}
+		}
+		return listaEnuns;
 	}
 	
 }
