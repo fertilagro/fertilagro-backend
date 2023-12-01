098566bcab5598d45323d7d24603d66b101b2dd7
TIAGO RIBEIRO <77054363+tiagoribas2021@users.noreply.github.com>
2023-12-01 15:56:04 -0300

    a2o

---
+++ src/main/resources/import.sql
@@ -0,0 +1,28 @@
+INSERT INTO cidade (
+  id, nome, codigo_ibge )
+VALUES
+(
+  1,
+  'Goiânia',
+  5200108
+),
+(
+  15,
+  'São Paulo',
+  3550308
+),
+(
+  468,
+  'Rio de Janeiro',
+  3300308
+),
+(
+  745,
+  'Goianira',
+  5205508
+),
+(
+  998,
+  'Santos',
+  3550308
+);
